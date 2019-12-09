a27c493e481ea68829ace644045ac8acfa1111a7
poymanov <n.poymanov@gmail.com>
2019-12-09 21:53:41 +0300

    Добавлены миграции для базовых таблиц

--- src/main/java/com/example/testauthapp/config/MvcConfig.java
+++ src/main/java/com/example/testauthapp/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.example.testauthapp;
+package com.example.testauthapp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
---
+++ src/main/resources/db/migration/V1__create_users.sql
@@ -0,0 +1,13 @@
+create table users
+(
+    id serial not null,
+    username varchar(255) not null,
+    password varchar(255) not null
+);
+
+create unique index users_id_uindex
+    on users (id);
+
+alter table users
+    add constraint users_pk
+        primary key (id);

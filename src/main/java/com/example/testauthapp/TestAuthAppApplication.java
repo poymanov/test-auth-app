09a72f72feeeae0cb0ccf8ebfc95ba8610d22c72
poymanov <n.poymanov@gmail.com>
2019-12-09 21:00:24 +0300

    Инициализация проекта

---
+++ src/main/java/com/example/testauthapp/TestAuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.testauthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAuthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAuthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testauthapp/TestAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testauthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
